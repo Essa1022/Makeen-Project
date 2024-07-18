7110788bf651066744fda6ae07a9e964163ba3e7
Essa1022 <ehsan.amz10@gmail.com>
2024-07-18 15:42:46 +0330

    media route, controller and validations

--- routes/media.php
+++ routes/media.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\MediaController;
 
 Route::group(['prefix' => 'media', 'as' => 'media.'], function () {
     Route::post('upload/{model_type}/{model_id}', [MediaController::class, 'upload'])->name('upload');
-    Route::delete('destroy/{model_type}/{model_id}', [MediaController::class, 'destroy'])->name('destroy');
-    Route::post('download/{model_type}/{model_id}', [MediaController::class, 'download'])->name('download')->withoutMiddleware('auth:sanctum');
+    Route::delete('destroy', [MediaController::class, 'destroy'])->name('destroy');
+    Route::post('download/{id}', [MediaController::class, 'download'])->name('download')->withoutMiddleware('auth:sanctum');
 });

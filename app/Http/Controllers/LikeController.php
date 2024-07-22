ada526f026dcd92f932358c3b2994d49df676fac
Essa1022 <ehsan.amz10@gmail.com>
2024-07-22 09:02:11 +0330

    edit LikeController.php and remove some routes

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,18 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\LikeRequest;
+use App\Http\Requests\Like\LikeRequest;
+use App\Http\Requests\Like\LikeRequest as LikeLikeRequest;
 use App\Models\Comment;
 use App\Models\Like;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    // Store a new Like
-    public function store(LikeRequest $request, Comment $comment)
+    public function like(LikeRequest $request, Comment $comment)
     {
         $type = $request->input('type');
         $ip_address = $request->ip();
+        $existing_like = Like::where('comment_id', $comment->id)
+            ->where('ip_address', $ip_address)
+            ->first();
+        if($existing_like)
+        {
+            if($existing_like->type == $type)
+            {
+                $existing_like->delete();
+                return $this->responseService->delete_response();
+            }
+            else
+            {
+                $existing_like->update(['type' => $type,]);
+                return $this->responseService->success_response($existing_like);
+            }
+        }
 
         $like = Like::create([
             'comment_id' => $comment->id,
@@ -21,35 +37,5 @@ class LikeController extends Controller
             'type' => $type,
         ]);
         return $this->responseService->success_response($like);
-    }
-
-    // Update Like
-    public function update(LikeRequest $request, Comment $comment)
-    {
-        $type = $request->input('type');
-        $ip_address = $request->ip();
-        $like = Like::where('comment_id', $comment->id)
-            ->where('ip_address', $ip_address)
-            ->first();
-        if ($like)
-        {
-        $like->update(['type' => $type,]);
-        return $this->responseService->success_response($like);
-        }
-    }
-
-    // Delete Like
-    public function delete(LikeRequest $request, Comment $comment)
-    {
-        $type = $request->input('type');
-        $ip_address = $request->ip();
-        $like = Like::where('comment_id', $comment->id)
-            ->where('ip_address', $ip_address)
-            ->first();
-        if($like && $like->type == $type)
-        {
-            $like->delete();
-            return $this->responseService->delete_response();
-        }
     }
 }
--- routes/like.php
+++ routes/like.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LikeController;
 
-Route::group(['prefix' => 'like', 'as' => 'like.'], function () {
-    Route::post('store/{comment}', [LikeController::class, 'store'])->name('store');
-    Route::put('update/{comment}', [LikeController::class, 'update'])->name('update');
-    Route::delete('delete/{comment}', [LikeController::class, 'delete'])->name('delete');
-})->withoutMiddleware('auth:sanctum');
+    Route::post('like/{comment}', [LikeController::class, 'like'])->name('like')->withoutMiddleware('auth:sanctum');
+

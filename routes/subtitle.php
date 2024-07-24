b76a99f9e3a6e1c5e7e608984db6fe20b420098d
Essa1022 <ehsan.amz10@gmail.com>
2024-07-24 14:18:10 +0330

    create subtitle routes , migration , model , controller and validation

---
+++ routes/subtitle.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\SubtitleController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::group(['prefix' => 'subtitles', 'as' => 'subtitles.'], function () {
+    Route::get('index', [SubtitleController::class, 'index'])->name('index');
+    Route::post('store', [SubtitleController::class, 'store'])->name('store');
+    Route::delete('destroy', [SubtitleController::class, 'destroy'])->name('destroy');
+});

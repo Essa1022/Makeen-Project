6f373453975d22df287c88f4c912a29451ed086c
Essa1022 <ehsan.amz10@gmail.com>
2024-07-28 15:22:24 +0330

    create article and media resources

---
+++ app/Http/Resources/Article/ArticleResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Article;
+
+use App\Http\Resources\Media\MediaResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'status' => $this->status,
+            'user_id' => $this->user_id,
+            'category_id' => $this->category_id,
+            'slug' => $this->slug,
+            'views' => $this->views,
+            'main_image_url' => MediaResource::collection($this->getMedia('main_image')),
+            'second_image_url' => MediaResource::collection($this->getMedia('second_image')),
+        ];
+    }
+}

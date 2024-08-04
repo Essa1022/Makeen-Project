ab8d5a5caa39751228e7490b4fe51466c05e4f49
Essa1022 <ehsan.amz10@gmail.com>
2024-08-04 15:45:14 +0330

    create, update and delete of category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Category\CreateCategoryRequest;
-use App\Http\Requests\Category\UpdateCategoryRequest;
+use App\Http\Requests\Category\CategoryRequest;
 use App\Http\Requests\User\UpdateUserRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -23,5 +22,49 @@ class CategoryController extends Controller
             $categories = Category::with('categories')->paginate(3);
         }
         return $this->responseService->success_response($categories);
+    }
+
+    // Store a new Category
+    public function store(CategoryRequest $request)
+    {
+        if ($request->user()->can('create.category'))
+        {
+            $category = Category::create($request->toArray());
+            return $this->responseService->success_response($category);
+        }
+        else
+        {
+            return $this->responseService->unauthorized_response();
+        }
+    }
+
+    // Update Category
+    public  function update(CategoryRequest $request, string $id)
+    {
+        if ($request->user()->can('update.category'))
+        {
+            $category = Category::find($id);
+            $category->update($request->toArray());
+            return $this->responseService->success_response($category);
+        }
+        else
+        {
+            return $this->responseService->unauthorized_response();
+        }
+    }
+
+    // Destroy Category
+    public function destroy(Request $request)
+    {
+        if ($request->user()->can('delete.category'))
+        {
+            $category_ids = $request->input('category_ids');
+            Category::destroy($category_ids);
+            return $this->responseService->delete_response();
+        }
+        else
+        {
+            return $this->responseService->unauthorized_response();
+        }
     }
 }
--- routes/category.php
+++ routes/category.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 
-Route::get('category/index/{category?}', [CategoryController::class, 'index'])->name('category.index')->withoutMiddleware('auth:sanctum');
+Route::group(['prefix' => 'categories', 'as' => 'categories.'], function () {
+    Route::get('index/{category?}', [CategoryController::class, 'index'])->name('index')->withoutMiddleware('auth:sanctum');
+    Route::post('store', [CategoryController::class, 'store'])->name('store');
+    Route::put('update/{id}', [CategoryController::class, 'update'])->name('update');
+    Route::delete('destroy', [CategoryController::class, 'destroy'])->name('destroy');
+});

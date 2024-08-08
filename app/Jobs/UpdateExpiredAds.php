2f26656c979e79e13abde5171a5197b09506301f
Essa1022 <ehsan.amz10@gmail.com>
2024-08-08 10:43:11 +0330

    job for update ads job

---
+++ app/Jobs/UpdateExpiredAds.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Ads;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateExpiredAds implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Ads::whereDate('ends_at', '<=', now())
+            ->update(['status' => 'inactive']);
+    }
+}

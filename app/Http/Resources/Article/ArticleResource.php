9ef3dc3e51fc3fbb36896f485a54a84801345854
Essa1022 <ehsan.amz10@gmail.com>
2024-09-03 15:36:04 +0330

    add special words to article resource

--- app/Http/Resources/Article/ArticleResource.php
+++ app/Http/Resources/Article/ArticleResource.php
@@ -20,6 +20,7 @@ class ArticleResource extends JsonResource
             'user_id' => $this->user_id,
             'slug' => $this->slug,
             'views' => $this->views,
+            'special_words' => $this->special_words,
             'categories' => CategoryResource::collection($this->categories),
             'main_image' => MediaResource::collection($this->getMedia('main_image')),
             'second_image' => MediaResource::collection($this->getMedia('second_image')),
